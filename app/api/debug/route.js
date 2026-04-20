50875b57fad50eea3331867ddd8410cc7bfe4d4f
Ethiq <james@ethiqrec.com>
2026-04-20 13:27:13 +0200

    fix: correct import path in debug endpoint

--- app/api/debug/route.js
+++ app/api/debug/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { fetchActiveRoles, fetchRoleDetail } from '../../../../lib/atlas.js'
+import { fetchActiveRoles, fetchRoleDetail } from '../../../lib/atlas.js'
 
 export async function GET() {
     try {
